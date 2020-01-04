438ca0ba0d5184f4ee92965f4785014be3f1adfb
arrathilar <wervvolf@gmail.com>
2020-01-04 11:19:34 +0200

    remake table to pureComponent

--- storefront/src/components/table/table.js
+++ storefront/src/components/table/table.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export class Table extends React.Component {
+export class Table extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
