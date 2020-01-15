e962a5dabcb612eac8669d376300cacc8a5660bf
Serhii Kozliuk <sergey.kozlyuk@gmail.com>
2020-01-15 15:31:38 +0200

    IV-17 confirm_order API view

--- purchase/urls.py
+++ purchase/urls.py
@@ -16,6 +16,6 @@ urlpatterns = (
     path("api/v1/get_cart/", api.GetCart.as_view()),
     path("api/v1/del_from_cart/<int:row>/<int:column>/", api.DelFromCart.as_view()),
     path("api/v1/update_quantity/<int:row>/<int:column>/<int:quantity>/", api.UpdateQuantity.as_view()),
-    # path("api/v1/confirm_order/", api.ConfirmOrder.as_view()),
+    path("api/v1/confirm_order/", api.ConfirmOrder.as_view()),
 
 )
