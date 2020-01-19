ceced82ab9465167dcfe9a6e3ba9e1076350cf99
Serhii Kozliuk <sergey.kozlyuk@gmail.com>
2020-01-19 12:35:25 +0200

    init_db updated

--- init_db.py
+++ init_db.py
@@ -2,8 +2,13 @@ from product.models import ProductInstance, DiopterPower, Cylinder, Product
 import random
 from datetime import date
 
-product = Product.objects.create(title='ІНТРАОКУЛЯРНА ЛІНЗА ENVISTA',
-                                 short_description='ІНТРАОКУЛЯРНАЯ ЛІНЗА ENVISTA',
+product = Product.objects.create(title='ІНТРАОКУЛЯРНА ЛІНЗА ENVISTA® TORIC',
+                                 short_description="""The Clear Choice for Exceptional Astigmatism Management.
+                                                      Exacting astigmatism correction. Dependable stability. Pristine visual clarity. It all comes together in the proven enVista platform to deliver:
+                                                      Aberration-free, glistening-free performance.
+                                                      Proven rotational stability.
+                                                      Exceptional outcomes.
+                                                      Delivered through an incision size as small as 2.2mm to limit potential SIA.""",
                                  product_image='product/enVista_920x632.png',
                                  brand_name='Bausch&Lomb',
                                  brand_image='brand/BL_logo.png',
