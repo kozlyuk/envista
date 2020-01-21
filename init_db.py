9270f33a4b44c9bd6448935ceae37146f893d578
Serhii Kozliuk <sergey.kozlyuk@gmail.com>
2020-01-21 11:56:44 +0200

    IV-30 Add permissions to init.py script

--- init_db.py
+++ init_db.py
@@ -1,6 +1,9 @@
-from product.models import ProductInstance, DiopterPower, Cylinder, Product
 import random
 from datetime import date
+
+from product.models import ProductInstance, DiopterPower, Cylinder, Product
+from django.contrib.auth.models import Group, Permission
+
 
 product = Product.objects.create(title='ІНТРАОКУЛЯРНА ЛІНЗА ENVISTA® TORIC',
                                  short_description="The Clear Choice for Exceptional Astigmatism Management. Exacting astigmatism correction. Dependable stability. Pristine visual clarity. It all comes together in the proven enVista platform to deliver:"
@@ -11,9 +14,6 @@ product = Product.objects.create(title='ІНТРАОКУЛЯРНА ЛІНЗА EN
                                  product_image='product/enVista_920x632.png',
                                  brand_name='Bausch&Lomb',
                                  brand_image='brand/BL_logo.png',
-                                 date_created=date.today(),
-                                 date_updated=date.today(),
-                                 created_by_id=1,
                                  footer="© 2020 ТОВ «Оптдіея». Усі права захищені."
                                 )
 
@@ -34,3 +34,49 @@ for column in Cylinder.objects.all():
                                        quantity_in_hand=random.randint(1,10),
                                        price=prices[prices_index])
     prices_index += 1
+
+
+def create_permission(group, model, permission):
+    new_group, created = Group.objects.get_or_create(name=group)
+    name = 'Can {} {}'.format(permission, model)
+    try:
+        model_add_perm = Permission.objects.get(name=name)
+    except Permission.DoesNotExist:
+        return print("Permission not found with name '{}'.".format(name))
+    new_group.permissions.add(model_add_perm)
+    return print("Added permission with name '{}'.".format(name))
+
+create_permission('Менеджери', 'user', 'view')
+create_permission('Менеджери', 'user', 'add')
+create_permission('Менеджери', 'user', 'change')
+create_permission('Менеджери', 'log entry', 'view')
+create_permission('Менеджери', 'Cylinder', 'view')
+create_permission('Менеджери', 'Diopter Power', 'view')
+create_permission('Менеджери', 'Product', 'view')
+create_permission('Менеджери', 'Product', 'change')
+create_permission('Менеджери', 'Product Instance', 'view')
+create_permission('Менеджери', 'Product Instance', 'change')
+create_permission('Менеджери', 'Order', 'view')
+create_permission('Менеджери', 'Order', 'add')
+create_permission('Менеджери', 'Order', 'change')
+create_permission('Менеджери', 'Order', 'delete')
+create_permission('Менеджери', 'order line', 'view')
+create_permission('Менеджери', 'order line', 'add')
+create_permission('Менеджери', 'order line', 'change')
+create_permission('Менеджери', 'order line', 'delete')
+create_permission('Менеджери', 'Purchase', 'view')
+create_permission('Менеджери', 'Purchase', 'add')
+create_permission('Менеджери', 'Purchase', 'change')
+create_permission('Менеджери', 'Purchase', 'delete')
+create_permission('Менеджери', 'purchase line', 'view')
+create_permission('Менеджери', 'purchase line', 'add')
+create_permission('Менеджери', 'purchase line', 'change')
+create_permission('Менеджери', 'purchase line', 'delete')
+
+create_permission('Клієнт', 'Order', 'view')
+create_permission('Клієнт', 'Order', 'add')
+create_permission('Клієнт', 'Order', 'change')
+create_permission('Клієнт', 'order line', 'view')
+create_permission('Клієнт', 'order line', 'add')
+create_permission('Клієнт', 'order line', 'change')
+create_permission('Клієнт', 'order line', 'delete')
