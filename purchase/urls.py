8e4e8c48edeaa18f9597eb0ac84746c887888efb
Serhii Kozliuk <sergey.kozlyuk@gmail.com>
2020-01-28 14:53:23 +0200

    Purchase urls added

--- purchase/urls.py
+++ purchase/urls.py
@@ -9,4 +9,9 @@ urlpatterns = (
     path("api/v1/update_quantity/<int:product_pk>/<int:quantity>/", api.UpdateQuantity.as_view()),
     path("api/v1/confirm_order/", api.ConfirmOrder.as_view()),
 
+    path("api/v1/get_purchase_table/", api.GetPurchaseTable.as_view()),
+    path("api/v1/get_purchase_list/", api.GetPurchaseList.as_view()),
+    path("api/v1/add_to_purchase/<int:row>/<int:column>/", api.AddToPurchase.as_view()),
+    path("api/v1/update_purchase_line/<int:product_pk>/<int:quantity>/", api.UpdatePurchaseLine.as_view()),
+    path("api/v1/confirm_purchase/", api.ConfirmPurchase.as_view()),
 )
