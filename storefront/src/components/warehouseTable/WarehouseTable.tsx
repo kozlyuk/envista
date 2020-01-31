d2d24e17f6864fae23d520f33cecf7ea4cc68fb8
arrathilar <wervvolf@gmail.com>
2020-01-31 16:19:34 +0200

    add sticky headers to warehouse table

--- storefront/src/components/warehouseTable/WarehouseTable.tsx
+++ storefront/src/components/warehouseTable/WarehouseTable.tsx
@@ -16,6 +16,8 @@ import Loader from "react-loader-spinner";
 import ButtonBackground from "../buttonBackground/buttonBackground";
 import {toast} from "react-toastify";
 import axios from "axios";
+import {Sticky, StickyContainer} from "react-sticky";
+import "./style.css"
 
 interface WarehouseTableProps {
 	getData: any;
@@ -118,53 +120,58 @@ export default class WarehouseTable extends Table implements WarehouseTableProps
 			return (
 				<div className="row">
 					<table className="table-bordered col mb-4 header-fixed">
-						<colgroup>
-							<col style={{width: 50, minWidth: 50}}/>
-						</colgroup>
-						<thead style={{position: "sticky"}} className="rc-table-thead text-center">
-						<tr>
-							<th className="rc-table-row-cell-break-word"/>
-							{this.state.columnsName.map((name: string, rowIdx: number) => (
-								<th key={rowIdx} className="rc-table-row-cell-break-word">
-									{name}
-								</th>
-							))}
-						</tr>
-						</thead>
-						<tbody className="rc-table-tbody">
-						{this.state.rows.map((item: { row: React.ReactNode; quantities: any[]; }, rowIdx: string | number | undefined) => (
-							<tr key={rowIdx}
-							    className="rc-table-row rc-table-row-level-0"
-							    data-row-key={1}>
-								<td className="rc-table-row-cell-break-word text-center">
+						<StickyContainer>
+							<colgroup>
+								<col style={{width: 50, minWidth: 50}}/>
+							</colgroup>
+							<Sticky style={{zIndex: 1000}}>{({style}) =>
+								<thead style={style} className="rc-table-thead sticky">
+								<tr>
+									<th style={{width: "57px"}} className="rc-table-row-cell-break-word"/>
+									{this.state.columnsName.map((name: string, rowIdx: number) => (
+										<th style={{width: "57px"}} key={rowIdx}
+										    className="rc-table-row-cell-break-word text-center bg-light">
+											{name}
+										</th>
+									))}
+								</tr>
+								</thead>
+							}</Sticky>
+							<tbody className="rc-table-tbody">
+							{this.state.rows.map((item: { row: React.ReactNode; quantities: any[]; }, rowIdx: string | number | undefined) => (
+								<tr key={rowIdx}
+								    className="rc-table-row rc-table-row-level-0"
+								    data-row-key={1}>
+									<td className="rc-table-row-cell-break-word text-center">
 									<span className="rc-table-row-indent indent-level-0"
 									      style={{paddingLeft: 0}}>{item.row}</span>
-								</td>
-								{item.quantities.map((counter, columnIdx) => (
-									<td
-										key={columnIdx}
-										className="rc-table-row-cell-break-word">
+									</td>
+									{item.quantities.map((counter, columnIdx) => (
+										<td
+											key={columnIdx}
+											className="rc-table-row-cell-break-word">
 											<span
 												className="rc-table-row-indent indent-level-0"
 												style={{paddingLeft: 0}}
 											/>
-										<div>
-											<ButtonBackground>
-												<button
-													style={{backgroundColor: "transparent"}}
-													className="btn btn-sm btn-light btn-block"
-													onClick={() =>
-														this.increaseQty(counter, columnIdx, rowIdx as number)
-													}>
-													{counter}
-												</button>
-											</ButtonBackground>
-										</div>
-									</td>
-								))}
-							</tr>
-						))}
-						</tbody>
+											<div>
+												<ButtonBackground>
+													<button
+														style={{backgroundColor: "transparent"}}
+														className="btn btn-sm btn-light btn-block"
+														onClick={() =>
+															this.increaseQty(counter, columnIdx, rowIdx as number)
+														}>
+														{counter}
+													</button>
+												</ButtonBackground>
+											</div>
+										</td>
+									))}
+								</tr>
+							))}
+							</tbody>
+						</StickyContainer>
 					</table>
 				</div>
 			);
