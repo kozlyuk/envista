03fb5deb90d0b865de67cf96d9a3955aa8660388
kozlyuk <serg@debian.serg.itel.rv.ua>
2020-02-04 11:26:22 +0200

    Webhook added

--- messaging/views.py
+++ messaging/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.http import HttpResponse
+from django.core.exceptions import PermissionDenied
+from django.views.decorators.csrf import csrf_exempt
+import telebot
+import logging
 
-# Create your views here.
+
+envista_bot = telebot.TeleBot(settings.TG_TOKEN)
+
+@csrf_exempt
+def bot(request):
+
+    if request.META['CONTENT_TYPE'] == 'application/json':
+
+        json_data = request.body.decode('utf-8')
+        update = telebot.types.Update.de_json(json_data)
+        envista_bot.process_new_updates([update])
+
+        return HttpResponse("")
+
+    raise PermissionDenied
+
+
+@envista_bot.message_handler(content_types=["text"])
+def get_okn(message):
+    envista_bot.send_message(message.chat.id, "Hello, bot!")
