92ac669b89d80780f19559cd90ee037c932f5633
arrathilar <wervvolf@gmail.com>
2020-04-24 12:26:30 +0300

    fix router

--- storefront/src/components/navbar/navbarMenu.js
+++ storefront/src/components/navbar/navbarMenu.js
@@ -18,105 +18,107 @@ import Nav from "react-bootstrap/Nav";
 import axios from "axios";
 
 class NavbarMenu extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			brand_image: null
-		};
-		this.user = new Auth();
-		this.authToken = this.user.getAuthToken();
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      brand_image: null
+    };
+    this.user = new Auth();
+    this.authToken = this.user.getAuthToken();
+  }
 
-	componentDidMount() {
-		axios(process.env.REACT_APP_INFO_CONTEXT, {
-			headers: {
-				Authorization: "Token " + this.authToken
-			}
-		}).then(
-			result => {
-				this.setState({
-					brand_image: result.data.brand_image
-				});
-			},
-			error => {
-				this.setState({
-					error
-				});
-			}
-		);
-		return void 0;
-	}
+  componentDidMount() {
+    axios(process.env.REACT_APP_INFO_CONTEXT, {
+      headers: {
+        Authorization: "Token " + this.authToken
+      }
+    }).then(
+      result => {
+        this.setState({
+          brand_image: result.data.brand_image
+        });
+      },
+      error => {
+        this.setState({
+          error
+        });
+      }
+    );
+    return void 0;
+  }
 
-	render() {
-		return (
-			<Fragment>
-				<div>
-					<Navbar collapseOnSelect expand="lg" className="top-nav">
-						<div className="container">
-							<Navbar.Brand href="#home"><Link to="/" className="mr-auto m-0 brand w-nav-brand p-0">
-								<img
-									src={this.state.brand_image}
-									height={50}
-									alt="brand"
-									className="image-2"
-								/>
-							</Link></Navbar.Brand>
-							<Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-							<Navbar.Collapse id="responsive-navbar-nav" className="menu-list">
-								<Nav className="ml-auto">
-									<Link style={{color: "#222222", padding: "20px"}} to="/">
-										Головна
-									</Link>
-									<a
-										href="https://envista.toriccalculator.com/(S(3p54nq0ludca5hrzdtsqdtfl))/UserAgreement.aspx"
-										target="_blank"
-										rel="noopener noreferrer"
-										className=""
-										style={{maxWidth: 940, color: "#222222", padding: "20px"}}>
-										Калькулятор
-									</a>
-									{this.props.userIsStaff ? <Link
-										style={{
-											maxWidth: 940,
-											color: "#222222",
-											display: "inline-block",
-											padding: "20px"
-										}}
-										to="/warehouse">
-										Поповнення складу
-									</Link> : ""}
-									<Link
-										style={{
-											maxWidth: 940,
-											color: "#222222",
-											display: "inline-block",
-											padding: "20px"
-										}}
-										to="/basket">
-										Корзина
-									</Link>
-									<Link
-										style={{
-											maxWidth: 940,
-											color: "#222222",
-											display: "inline-block",
-											padding: "20px"
-										}}
-										to="/logout"
-										onClick={() => {
-											this.user.logout();
-										}}>
-										Вийти ({this.props.userEmail})
-									</Link>
-								</Nav>
-							</Navbar.Collapse>
-						</div>
-					</Navbar>
-				</div>
+  render() {
+    return (
+      <Fragment>
+        <div>
+          <Navbar collapseOnSelect expand="lg" className="top-nav">
+            <div className="container">
+              <Navbar.Brand href="#home"><Link to="/" className="mr-auto m-0 brand w-nav-brand p-0">
+                <img
+                  src={this.state.brand_image}
+                  height={50}
+                  alt="brand"
+                  className="image-2"
+                />
+              </Link></Navbar.Brand>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+              <Navbar.Collapse id="responsive-navbar-nav" className="menu-list">
+                <Nav className="ml-auto">
+                  <Link style={{color: "#222222", padding: "20px"}} to="/">
+                    Головна
+                  </Link>
+                  <a
+                    href="https://envista.toriccalculator.com/(S(3p54nq0ludca5hrzdtsqdtfl))/UserAgreement.aspx"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className=""
+                    style={{maxWidth: 940, color: "#222222", padding: "20px"}}>
+                    Калькулятор
+                  </a>
+                  {this.props.userIsStaff ? <Link
+                    style={{
+                      maxWidth: 940,
+                      color: "#222222",
+                      display: "inline-block",
+                      padding: "20px"
+                    }}
+                    to="/warehouse">
+                    Поповнення складу
+                  </Link> : ""}
+                  <Link
+                    style={{
+                      maxWidth: 940,
+                      color: "#222222",
+                      display: "inline-block",
+                      padding: "20px"
+                    }}
+                    to="/basket">
+                    Корзина
+                  </Link>
+                  <Link
+                    style={{
+                      maxWidth: 940,
+                      color: "#222222",
+                      display: "inline-block",
+                      padding: "20px"
+                    }}
+                    to="/logout"
+                    onClick={() => {
+                      this.user.logout().then(() => {
+                        window.location.assign('/');
+                      });
+                    }}>
+                    Вийти ({this.props.userEmail})
+                  </Link>
+                </Nav>
+              </Navbar.Collapse>
+            </div>
+          </Navbar>
+        </div>
 
-			</Fragment>
-		);
-	}
+      </Fragment>
+    );
+  }
 }
 
 export default NavbarMenu;
