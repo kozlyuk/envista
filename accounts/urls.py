9b87ffd784d70f6ea3c1746b4166917d7b5a2520
kozlyuk <sergey.kozlyuk@gmail.com>
2020-04-27 09:03:59 +0300

    EV-63 Created GetOrders API

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from accounts.api import UserDetailsView, Register, Activate
+from accounts.api import UserDetailsView, Register, Activate, GetOrders
 
 urlpatterns = (
-    path("api/v1/User/", UserDetailsView.as_view()),
+    path("api/v1/user/", UserDetailsView.as_view()),
+
+    path("api/v1/get_orders/<str:customer>/", GetOrders.as_view(), name='get_orders'),
 
     path("api/v1/register/", Register.as_view(), name='register'),
     path("api/v1/activate/<str:uidb64>/<str:token>/", Activate.as_view(), name='activate'),
